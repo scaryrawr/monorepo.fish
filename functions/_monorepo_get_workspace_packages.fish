2df614a7cf3e908efcd1c17e6c9d19b5c8416a4c
Mike Wallio <michael.wallio@outlook.com>
2025-02-20 10:39:13 -0500

    Use temp files to get around OS argument limits

--- functions/_monorepo_get_workspace_packages.fish
+++ functions/_monorepo_get_workspace_packages.fish
@@ -1,20 +1,26 @@
 function _monorepo_get_workspace_packages
-    set -l packages "[]"
+    set -l packages_file (mktemp)
+
+    echo "[]" >$packages_file
 
     if test -f "./package.json"
         set -l yarn_packages (_monorepo_search_yarn_workspace)
-        set packages (jq -n --argjson existing_packages "$packages" --argjson new_packages "$yarn_packages" '$existing_packages + $new_packages')
+        echo $yarn_packages | jq -s '.[0] + .[1]' $packages_file - >$packages_file.tmp
+        mv $packages_file.tmp $packages_file
     end
 
     if test -f "./Cargo.toml"
         set -l cargo_packages (_monorepo_search_cargo_workspace)
-        set packages (jq -n --argjson existing_packages "$packages" --argjson new_packages "$cargo_packages" '$existing_packages + $new_packages')
+        echo $cargo_packages | jq -s '.[0] + .[1]' $packages_file - >$packages_file.tmp
+        mv $packages_file.tmp $packages_file
     end
 
-    if test (echo $packages | jq 'length') -eq 0
+    if test (jq 'length' $packages_file) -eq 0
         echo "No supported workspace configuration found."
+        rm $packages_file
         return 1
     end
 
-    echo $packages
+    cat $packages_file
+    rm $packages_file
 end
