2ada397000946fa8a72fb895ba1f88eecae2b756
Mike Wallio <michael.wallio@outlook.com>
2025-05-02 19:37:42 -0400

    Redirect errors

--- functions/_monorepo_search_cargo_workspace.fish
+++ functions/_monorepo_search_cargo_workspace.fish
@@ -1,4 +1,4 @@
 # Searches for Cargo workspace packages and outputs name/path information.
 function _monorepo_search_cargo_workspace
-    cargo metadata --format-version 1 | jq -r '.packages | map(select(.id | startswith("path+file")) | {name: .name, path: .manifest_path})'
+    cargo metadata --format-version 1 2>/dev/null | jq -r '.packages | map(select(.id | startswith("path+file")) | {name: .name, path: .manifest_path})'
 end
--- functions/_monorepo_search_yarn_workspace.fish
+++ functions/_monorepo_search_yarn_workspace.fish
@@ -1,6 +1,6 @@
 # Retrieves Yarn workspace package information.
 function _monorepo_search_yarn_workspace
-    yarn --json workspaces info | jq -r '.data' | jq -r '
+    yarn --json workspaces info 2>/dev/null | jq -r '.data' | jq -r '
         to_entries | map({name: .key, path: ("./" + .value.location + "/package.json")})
     '
 end
