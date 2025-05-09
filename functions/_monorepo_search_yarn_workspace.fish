c226583bf9eda0c200733c931067cd3749675c2b
Mike Wallio <michael.wallio@outlook.com>
2025-05-09 16:17:56 -0400

    Add support for yarn 4

--- functions/_monorepo_search_yarn_workspace.fish
+++ functions/_monorepo_search_yarn_workspace.fish
@@ -1,6 +1,12 @@
 # Retrieves Yarn workspace package information.
 function _monorepo_search_yarn_workspace
-    yarn --json workspaces info 2>/dev/null | jq -r '.data' | jq -r '
-        to_entries | map({name: .key, path: ("./" + .value.location + "/package.json")})
-    '
+    set -l yarn_version (yarn --version)
+    # Check if we're using Yarn 1.x
+    if string match -q "1.*" $yarn_version
+        yarn --json workspaces info 2>/dev/null | jq -r '.data' | jq -r '
+            to_entries | map({name: .key, path: (.value.location + "/package.json")})
+        '
+    else
+        yarn workspaces list --json 2>/dev/null | jq -s '[.[] | select(.location != ".") | {name: .name, path: (.location + "/package.json")}]'
+    end
 end
