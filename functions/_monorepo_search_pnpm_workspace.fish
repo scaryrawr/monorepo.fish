03ae233ad35ed45e5a7836e4196e849f199b4e87
Mike Wallio <michael.wallio@outlook.com>
2025-08-19 21:25:09 -0400

    Add check for pnpm

--- functions/_monorepo_search_pnpm_workspace.fish
+++ functions/_monorepo_search_pnpm_workspace.fish
@@ -1,6 +1,6 @@
 # Searches for pnpm workspace packages and outputs name/path information.
 function _monorepo_search_pnpm_workspace
-    if test -f "./pnpm-workspace.yaml"
+    if test -f "./pnpm-workspace.yaml"; and command -q pnpm
         set -l raw_output (pnpm list --recursive --depth -1 --json 2>/dev/null)
         
         # Filter out root package and convert paths to relative
