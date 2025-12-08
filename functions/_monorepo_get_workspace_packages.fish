228fd5bb1603da7a133be180e4e015ca6c291e62
Mike Wallio <michael.wallio@outlook.com>
2025-12-08 10:26:37 -0500

    refactor: simplify workspace package handling with tab-separated output
    - Replace JSON output format with tab-separated values (name\tpath)
    - Move config file existence checks into workspace search functions
    - Simplify preview function to receive path directly instead of re-querying
    - Update fzf arguments to use --delimiter and --with-nth for cleaner display
    - Remove unused helper functions (_monorepo_extract_package_names, _monorepo_validate_packages)

--- functions/_monorepo_get_workspace_packages.fish
+++ functions/_monorepo_get_workspace_packages.fish
@@ -1,14 +1,5 @@
 # Retrieves workspace packages from the monorepo, caching results to optimize repeated lookups.
 function _monorepo_get_workspace_packages
-    set -l node_packages '[]'
-    if test -f "./package.json"
-        set node_packages (_monorepo_search_node_workspace)
-    end
-
-    set -l cargo_packages '[]'
-    if test -f "./Cargo.toml"
-        set cargo_packages (_monorepo_search_cargo_workspace)
-    end
-
-    echo "$node_packages" "$cargo_packages" | jq -s '.[0] + .[1]'
+    _monorepo_search_node_workspace
+    _monorepo_search_cargo_workspace
 end
--- functions/_monorepo_preview_package_path.fish
+++ functions/_monorepo_preview_package_path.fish
@@ -1,16 +1,8 @@
 # Previews the path(s) of a workspace package and warns if multiple packages share the same name.
 function _monorepo_preview_package_path
-    set -f packages (_monorepo_get_workspace_packages)
-
     set -l package_name $argv[1]
-    set -l package_paths (echo $packages | jq -r ".[] | select(.name == \"$package_name\") | .path")
+    set -l package_path $argv[2]
 
-    if test (count $package_paths) -gt 1
-        echo (set_color yellow)"Warning: multiple packages with the same name found"(set_color normal)
-    end
-
-    for package_path in $package_paths
-        echo $package_path
-        _fzf_preview_file "$package_path"
-    end
+    echo $package_name
+    _fzf_preview_file "$package_path"
 end
