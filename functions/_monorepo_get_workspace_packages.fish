e053610446483ba815be50006e7d8ccf4588f6a0
Mike Wallio <michael.wallio@outlook.com>
2025-12-09 12:41:52 -0500

    Allow workspace listing to run in parallel

--- functions/_monorepo_get_workspace_packages.fish
+++ functions/_monorepo_get_workspace_packages.fish
@@ -1,5 +1,7 @@
 # Retrieves workspace packages from the monorepo, caching results to optimize repeated lookups.
+# Runs workspace searches in parallel for faster fzf streaming.
 function _monorepo_get_workspace_packages
-    _monorepo_search_node_workspace
-    _monorepo_search_cargo_workspace
+    _monorepo_search_node_workspace &
+    _monorepo_search_cargo_workspace &
+    wait
 end
